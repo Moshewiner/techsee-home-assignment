5a1f238d8b4e7fe045f021217c182fc2657dc9b7
moshew <Moshewiner13@gmail.com>
2020-06-05 16:40:50 +0300

    added react-table library and configured as the first example of it

---
+++ src/pages/home/home.page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Table from '../../components/table/table.component';
+
+function HomePage() {
+    return (
+        <Table></Table>
+    );
+}
+
+export default HomePage;
